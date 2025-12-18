49457c6dd54be0cdccaf01e9321eaae332e6bfd1
Irfan Kazi <irfangithub123@gmail.com>
2025-12-18 14:43:36 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import{ connectDB } from "./db/connection.js"
+import { app } from "./app.js"
+
+connectDB().then(()=>{
+    app.listen(process.env.PORT||3000,()=>{
+        console.log(`Server is running On ${process.env.PORT}`);
+    })
+}).catch((err)=>console.log(err))
